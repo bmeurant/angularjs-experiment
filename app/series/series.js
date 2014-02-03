21772e57f2df4a61886a328578d6781d990f3376
Baptiste Meurant <baptiste.meurant@gmail.com>
2014-02-03 12:39:49 +0100

    move series states into series module

--- app/series/series.js
+++ app/series/series.js
@@ -1,5 +1,29 @@
 angular.module('series', ['resources.series'])
 
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('series', {
+                url: "/series",
+                templateUrl: 'series/series-list.html',
+                controller: 'seriesListViewCtrl',
+                resolve: {
+                    series: ['Series', function (Series) {
+                        return Series.query();
+                    }]
+                }
+            })
+            .state('series.detail', {
+                url: "/{id:[0-9]{1,4}}",
+                templateUrl: "series/series-detail.html",
+                controller: 'seriesDetailViewCtrl',
+                resolve: {
+                    seriesItem: ['Series', '$stateParams', function (Series, $stateParams) {
+                        return Series.get({id: $stateParams.id});
+                    }]
+                }
+            })
+    })
+
     .controller('seriesListViewCtrl', ['$scope', 'series', function ($scope, series) {
         $scope.series = series;
     }])
