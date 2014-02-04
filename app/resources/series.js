69d29fab344726cb839f3e34f5ebe7d249257da7
Baptiste Meurant <baptiste.meurant@gmail.com>
2014-02-04 12:02:54 +0100

    move to a shared model to implement two-way bindings between list and edit view

--- app/resources/series.js
+++ app/resources/series.js
@@ -1,17 +1,31 @@
 var seriesResource = angular.module('resources.series', ['ngResource', 'resources.albums']);
 
 seriesResource.factory('Series', ['$resource', 'Albums', function ($resource, Albums) {
+
+    var fillAlbums = function(seriesItem) {
+        if (Array.isArray(seriesItem.albums)) {
+            var fullAlbums = [];
+            seriesItem.albums.forEach(function (item) {
+                fullAlbums.push(Albums.get({id: item}));
+            });
+            seriesItem.albums = fullAlbums;
+        }
+    }
+
     var res = $resource('series/:id', {id: '@id'}, {
-        query: {method: 'GET', isArray: true},
+        query: {method: 'GET', isArray: true,
+            transformResponse: function (series) {
+                if (Array.isArray(series)) {
+                    for (var index = 0; index < series.length; ++index) {
+                        fillAlbums(series[index]);
+                    }
+                }
+                return series;
+            }.bind(this)
+        },
         get: {method: 'GET',
             transformResponse: function (seriesItem) {
-                if (Array.isArray(seriesItem.albums)) {
-                    var fullAlbums = [];
-                    seriesItem.albums.forEach(function (item) {
-                        fullAlbums.push(Albums.get({id: item}));
-                    });
-                    seriesItem.albums = fullAlbums;
-                }
+                fillAlbums(seriesItem);
                 return seriesItem;
             }.bind(this)
         }});
--- app/series/series.js
+++ app/series/series.js
@@ -5,23 +5,13 @@ angular.module('series', ['resources.series', 'resources.albums'])
             .state('series', {
                 url: "/series",
                 templateUrl: 'series/series-list.html',
-                controller: 'seriesListCtrl',
-                resolve: {
-                    series: ['Series', function (Series) {
-                        return Series.query();
-                    }]
-                }
+                controller: 'seriesListCtrl'
             })
             .state('series.item', {
                 abstract: true,
                 url: '/{id:[0-9]{1,4}}',
                 templateUrl: "series/series-item.html",
-                controller: 'seriesItemCtrl',
-                resolve: {
-                    seriesItem: ['Series', '$stateParams', function (Series, $stateParams) {
-                        return Series.get({id: $stateParams.id});
-                    }]
-                }
+                controller: 'seriesItemCtrl'
             })
             .state('series.item.detail', {
                 url: "",
@@ -34,19 +24,57 @@ angular.module('series', ['resources.series', 'resources.albums'])
             })
     })
 
-    .controller('seriesListCtrl', ['$scope', 'series', function ($scope, series) {
-        $scope.series = series;
-    }])
+    .factory('SeriesModel', function($q, Series){
+        var Model = function(Series){
+            this.resource = Series;
+            this.data = $q.defer();
+            this.fetch();
+        };
 
-    .controller('seriesItemCtrl', ['$scope', '$state', 'seriesItem', function ($scope, $state, seriesItem) {
-        $scope.seriesItem = seriesItem;
+        Model.prototype.fetch = function(){
+            Series.query().$promise.then(function(result) {
+                this.data.resolve(result);
+            }.bind(this));
+        };
+
+        Model.prototype.all = function(){
+            return this.data.promise;
+        };
+
+        Model.prototype.get = function(id){
+            var itemData = $q.defer();
+            this.data.promise.then(function(data) {
+                itemData.resolve(data[id - 1]);
+            }.bind(this));
+            return itemData.promise;
+        };
+
+        return new Model(Series);
+    })
+
+    .controller('seriesListCtrl', function ($scope, SeriesModel) {
+        SeriesModel.all().then( function (series) {
+            $scope.series = series;
+        });
+
+        $scope.fetch = function(){
+            SeriesModel.fetch();
+        };
+    })
+
+    .controller('seriesItemCtrl', ['$scope', '$state', '$stateParams', 'SeriesModel', function ($scope, $state, $stateParams, SeriesModel) {
+        SeriesModel.get($stateParams.id).then( function (seriesItem) {
+            $scope.seriesItem = seriesItem;
+        });
         $scope.edit = function () {
             $state.go('series.item.edit');
         }
     }])
 
-    .controller('seriesItemEditCtrl', ['$scope', '$state', 'seriesItem', function ($scope, $state, seriesItem) {
-        $scope.seriesItem = seriesItem;
+    .controller('seriesItemEditCtrl', ['$scope', '$state', '$stateParams', 'SeriesModel', function ($scope, $state, $stateParams, SeriesModel) {
+        SeriesModel.get($stateParams.id).then( function (seriesItem) {
+            $scope.seriesItem = seriesItem;
+        });
         $scope.cancel = function () {
             $state.go('series.item.detail');
         }
