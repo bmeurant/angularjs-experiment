3726144dd7b666f1784919c45c9b2a838bd1426c
Baptiste Meurant <baptiste.meurant@gmail.com>
2014-02-12 00:43:27 +0100

    use a real REST backend

--- app/resources/prod/albums.js
+++ app/resources/prod/albums.js
@@ -1,5 +1,5 @@
 var seriesResource = angular.module('resources.albums', ['ngResource']);
 
 seriesResource.factory('Albums', ['$resource', function ($resource) {
-    return $resource('albums/:id', {id: '@id'});
+    return $resource('http://localhost:8080/api/albums/:id', {id: '@id'});
 }]);
--- app/resources/prod/series.js
+++ app/resources/prod/series.js
@@ -3,10 +3,23 @@ var seriesResource = angular.module('resources.series', ['ngResource']);
 seriesResource.factory('Series', ['$resource', function ($resource) {
 
     var res = $resource('http://localhost:8080/api/series/:id', {id: '@id'}, {
-            query: {method: 'GET', isArray: true},
+            query: {
+                method: 'GET',
+                transformResponse: function (data) {
+                    return JSON.parse(data).content;
+                },
+                isArray: true
+            },
             get: {method: 'GET'},
             save: {method:'POST'},
-            update: {method:'PUT'}
+            update: {
+                method:'PUT',
+                transformRequest: function (data) {
+                    data.albums = null;
+                    delete data.validations;
+                    return JSON.stringify(data);
+                }
+            }
         }
     );
 
